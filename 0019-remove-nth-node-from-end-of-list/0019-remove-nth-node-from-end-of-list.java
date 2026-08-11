8b63dd304e9083910d7f8629871ae7b3af68b87d
anikdotio <kapatak47@gmail.com>
2026-08-11 12:21:00 +0530

    Time: 0 ms (100%), Space: 43.6 MB (40.44%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,39 +1,67 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
+ *
  *     int val;
+ *
  *     ListNode next;
+ *
  *     ListNode() {}
+ *
  *     ListNode(int val) { this.val = val; }
+ *
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *
  * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head.next == null){
-            return null;
-        }
+
+        // if(head.next == null){
+        //     return null;
+        // }
 
         int size = 0;
         ListNode currNode = head;
-        while(currNode!= null){
+
+        while(currNode != null){
             currNode = currNode.next;
             size++;
         }
 
-        if(n == size){
-            return head.next;
-        }
+        // if(n == size){
+        //     return head.next;
+        // }
 
-        int indexToSearch = size - n;
-        int i=1;
-        ListNode prev = head;
-        while(i< indexToSearch){
+        // int indexToSearch = size - n;
+        // int i=1;
+        // ListNode prev = head;
+        // while(i< indexToSearch){
+        //     prev = prev.next;
+        //     i++;
+        // }
+
+        // prev.next = prev.next.next;
+        // return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        int d = size - n + 1;
+
+        ListNode prev = dummy,
+                 curr = head;
+
+        int i = 0;
+
+        while(i < d - 1){
+            curr = curr.next;
             prev = prev.next;
             i++;
         }
 
-        prev.next = prev.next.next;
-        return head;
+        prev.next = curr.next;
+
+        return dummy.next;
     }
 }
