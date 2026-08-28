b2815bbd210d894411f78321caec9f9cdd96f70c
anikdotio <kapatak47@gmail.com>
2026-08-28 22:04:04 +0530

    Time: 1772 ms (20.89%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+-- SubQuery Approach
+SELECT customer_id, COUNT(DISTINCT visit_id) AS count_no_trans FROM Visits
+WHERE visit_id NOT IN(
+    SELECT visit_id FROM Transactions
+)
+GROUP BY customer_id
